0b79a7dbc5d03380d080a52b701c659f09b3a97a
Jaroslav Pernica <jaroslav.pernica@olomouc.eu>
2017-12-01 12:50:26 +0100

    Add moves and basic rules

--- Sources/draughts/main.swift
+++ Sources/draughts/main.swift
@@ -1,4 +1,5 @@
-let initialBoard: Array<Array<Int>> = [
+typealias Board = Array<Array<Int>>
+let initialBoard: Board = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -9,9 +10,10 @@ let initialBoard: Array<Array<Int>> = [
     [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-let pieces: [Character] = ["K", "M", ".", "m", "k"]
+typealias PieceNames = [Character]
+let pieces: PieceNames = ["K", "M", ".", "m", "k"]
 
-func printBoard(_ board: Array<Array<Int>>, with pieces: Array<Character>) -> Void {
+func printBoard(_ board: Board, with pieces: PieceNames = pieces) -> Void {
     var rowNum: Int = 8
 
     for row in board {
@@ -28,5 +30,51 @@ func printBoard(_ board: Array<Array<Int>>, with pieces: Array<Character>) -> Vo
     print("   A  B  C  D  E  F  G  H")
 }
 
-printBoard(initialBoard, with: pieces)
+enum Player {
+    case white, black
+}
+
+// printBoard(initialBoard)
+typealias Coordinate = (col: Int, row: Int)
+typealias Vector = (from: Coordinate, to: Coordinate)
+
+struct Move {
+    let player: Player
+    let vector: Vector
+}
+
+let move1 = Move(
+        player: Player.white,
+        vector: ((0, 2), (1, 3))
+)
+
+func pieceExists(_ board: Board,_ move: Move) -> Bool {
+    let coord = move.vector.from
+    let piece = board[coord.row][coord.col]
+
+    if piece == 0 {
+        return false
+    } else if move.player == Player.white && piece > 0 {
+        return false
+    } else if move.player == Player.black && piece < 0 {
+        return false
+    }
+
+    return true
+}
+
+func endSquareIsEmpty(_ board: Board, _ move: Move) -> Bool {
+    let coord = move.vector.to
+    let square = board[coord.row][coord.col]
+
+    if square == 0 {
+        return true
+    }
+
+    return false
+}
+
+
+
+
 
