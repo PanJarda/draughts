31c28ba551742cc51c727af2bc2c797a9aab0177
Jaroslav Pernica <jaroslav.pernica@olomouc.eu>
2017-12-01 12:01:50 +0100

    Add board printing

--- Sources/draughts/main.swift
+++ Sources/draughts/main.swift
@@ -1,6 +1,32 @@
-struct draughts {
-    var text = "Hello, World!"
+let initialBoard: Array<Array<Int>> = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+let pieces: [Character] = ["K", "M", ".", "m", "k"]
+
+func printBoard(_ board: Array<Array<Int>>, with pieces: Array<Character>) -> Void {
+    var rowNum: Int = 8
+
+    for row in board {
+        print("\(rowNum) ", terminator: "")
+
+        for square in row {
+            print(" \(pieces[square + 2]) ", terminator: "")
+        }
+
+        print("")
+        rowNum -= 1
+    }
+
+    print("   A  B  C  D  E  F  G  H")
 }
 
-print("ahoj")
+printBoard(initialBoard, with: pieces)
 
